76223a193ac26393b6490ef30ec6f264304426e5
Skip McGee <70141823+skipmcgee@users.noreply.github.com>
2020-10-15 17:07:54 +0300

    Update weatherscraper.py

--- weatherscraper.py
+++ weatherscraper.py
@@ -9,7 +9,6 @@
 
 import logging
 import logging.handlers
-
 from bs4 import BeautifulSoup
 import time
 from threading import Thread
@@ -20,27 +19,33 @@ import datetime
 from PIL import ImageTk, Image
 
 
-global error_count
+global error_count, counter
+
+counter = 0
 
 def timer(delay):
     rightnow = datetime.datetime.today()
     time.sleep(delay)
-
-
-def infoscraper(url):
-    error_count = 0
-    try:
+    counter += 1
+    if counter is 1 or 2:
+        newtime = datetime.datetime.today()
+        timer(delay)
+    if counter >= 2:
+        raise TimeoutError
 #        thread1 = Thread(target=timer(15), daemon=True)
 #        thread1.start()
 #        timer(delay=10)
 
-        json_url = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=40.9369&lon=14.0334&altitude=15"
-        page = requests.get(json_url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        print(soup)
-    except Exception as error:
-        print(error)
-        error_count += 1
+#def infoscraper(url):
+#    error_count = 0
+#    try:
+#        json_url = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=40.9369&lon=14.0334&altitude=15"
+#        page = requests.get(json_url)
+#        soup = BeautifulSoup(page.content, 'html.parser')
+#        print(soup)
+#    except Exception as error:
+#        print(error)
+#        error_count += 1
 
 def weatherscraper(url):
     error_count = 0
@@ -48,7 +53,7 @@ def weatherscraper(url):
     try:
         root = Tk()
         root.title("Weather App")
-        root.geometry("450x525")
+        root.geometry("450x550")
         root['background'] = "white"
 
         # Image
@@ -56,38 +61,27 @@ def weatherscraper(url):
         panel = Label(root, image=new)
         panel.place(x=350, y=75)
 
-
         # Dates
         dt = datetime.datetime.now()
-        date = Label(root, text=dt.strftime('%A--'), bg='white', font=("bold", 15))
-        date.place(x=5, y=130)
-        month = Label(root, text=dt.strftime('%m %B'), bg='white', font=("bold", 15))
-        month.place(x=100, y=130)
+        date = Label(root, text=dt.strftime('%A, %C %B, %Y'), bg='white', font=("bold", 15))
+        date.place(x=5, y=160)
 
         # Time
         hour = Label(root, text=dt.strftime('%I : %M %p'),
                      bg='white', font=("bold", 15))
-        hour.place(x=10, y=160)
+        hour.place(x=10, y=130)
 
-        # Theme for the respective time the application is used
-        if int((dt.strftime('%I'))) >= 8 & int((dt.strftime('%I'))) <= 5:
-            img = ImageTk.PhotoImage(Image.open('moon.png'))
-            panel = Label(root, image=img)
-            panel.place(x=210, y=200)
-        else:
-            img = ImageTk.PhotoImage(Image.open('sun.png'))
-            panel = Label(root, image=img)
-            panel.place(x=210, y=200)
+
+
+
 
 
         # City Search
         city_name = StringVar()
-        city_entry = Entry(root, textvariable=city_name, width=42)
+        city_entry = Entry(root, textvariable=city_name, width=63)
         city_entry.grid(row=1, column=0, ipady=10, stick=W+E+N+S)
 
-
         def city_name():
-
             # API Call
             api_key = "x"
             api_request = requests.get("https://api.openweathermap.org/data/2.5/weather?q="
@@ -100,6 +94,34 @@ def weatherscraper(url):
             humidity = y['humidity']
             tempmin = y['temp_min']
             tempmax = y['temp_max']
+
+            # Theme for current sun/rain status:
+            if humidity >= 90:
+                img = ImageTk.PhotoImage(Image.open('rain.png'))
+                panel = Label(root, image=img)
+                panel.place(x=1, y=200)
+            elif humidity >= 40 and humidity <= 89:
+                img = ImageTk.PhotoImage(Image.open('mixed.png'))
+                panel = Label(root, image=img)
+                panel.place(x=1, y=200)
+            elif humidity <= 39:
+                img = ImageTk.PhotoImage(Image.open('sunny.png'))
+                panel = Label(root, image=img)
+                panel.place(x=1, y=200)
+
+            # Theme for the respective time the application is used
+            if int((dt.strftime('%H'))) >= 20:
+                img = ImageTk.PhotoImage(Image.open('moon.png'))
+                panel = Label(root, image=img)
+                panel.place(x=210, y=200)
+            elif int((dt.strftime('%H'))) <= 7:
+                img = ImageTk.PhotoImage(Image.open('moon.png'))
+                panel = Label(root, image=img)
+                panel.place(x=210, y=200)
+            else:
+                img = ImageTk.PhotoImage(Image.open('sun.png'))
+                panel = Label(root, image=img)
+                panel.place(x=210, y=200)
 
             # Coordinates
             x = api['coord']
@@ -114,20 +136,19 @@ def weatherscraper(url):
             # Adding the received info into the screen
             lable_temp.configure(text=current_temperature)
             lable_humidity.configure(text=humidity)
-            max_temp.configure(text=tempmax)
+            max_temp.configure(text="Max Temp: " + tempmax)
             min_temp.configure(text=tempmin)
             lable_lon.configure(text=longtitude)
             lable_lat.configure(text=latitude)
             lable_country.configure(text=country)
             lable_citi.configure(text=citi)
 
-
         # Search Bar and Button
-        city_nameButton = Button(root, text="Search", command=city_name)
+        city_nameButton = Button(root, text="  Search  ", command=city_name)
         city_nameButton.grid(row=1, column=1, padx=4, stick=W+E+N+S)
 
-
         # Country  Names and Coordinates
+
         lable_citi = Label(root, text="...", width=0,
                            bg='white', font=("bold", 15))
         lable_citi.place(x=10, y=63)
@@ -139,52 +160,56 @@ def weatherscraper(url):
         lable_lon = Label(root, text="...", width=0,
                           bg='white', font=("Helvetica", 15))
         lable_lon.place(x=25, y=95)
+
         lable_lat = Label(root, text="...", width=0,
                           bg='white', font=("Helvetica", 15))
         lable_lat.place(x=95, y=95)
 
         # Current Temperature
-
-        lable_temp = Label(root, text="...", width=0, bg='white',
-                           font=("Helvetica", 110), fg='black')
-        lable_temp.place(x=18, y=220)
+        lable_temp = Label(root, text="ERROR", width=0, bg=None,
+                           font=("Helvetica", 90), fg='black')
+        lable_temp.place(x=5, y=245)
 
         # Other temperature details
-
         humi = Label(root, text="Humidity: ", width=0,
                      bg='white', font=("bold", 15))
-        humi.place(x=3, y=400)
+        humi.place(x=3, y=430)
 
         lable_humidity = Label(root, text="...", width=0,
                                bg='white', font=("bold", 15))
-        lable_humidity.place(x=107, y=400)
+        lable_humidity.place(x=107, y=430)
 
 
         maxi = Label(root, text="Max. Temp: ", width=0,
                      bg='white', font=("bold", 15))
-        maxi.place(x=3, y=430)
+        maxi.place(x=3, y=460)
 
         max_temp = Label(root, text="...", width=0,
                          bg='white', font=("bold", 15))
-        max_temp.place(x=128, y=430)
+        max_temp.place(x=128, y=460)
 
 
         mini = Label(root, text="Min. Temp: ", width=0,
                      bg='white', font=("bold", 15))
-        mini.place(x=3, y=460)
+        mini.place(x=3, y=490)
 
         min_temp = Label(root, text="...", width=0,
                          bg='white', font=("bold", 15))
-        min_temp.place(x=128, y=460)
-
+        min_temp.place(x=128, y=490)
 
         # Note
         note = Label(root, text="All temperatures in degrees Fahrenheit",
                      bg='white', font=("italic", 10))
-        note.place(x=95, y=495)
+        note.place(x=95, y=520)
+
+
 
 
         root.mainloop()
+
+
+    except ConnectionError:
+        error_count += 1
 
 
     except Exception as error:
@@ -203,7 +228,6 @@ def weatherscraper(url):
 
 def main():
     url = "https://weather.com/weather/hourbyhour/l/bd6e61a96a73fe700823357bc4695a0342074429b43fdbee1202ed754b361eee"
-    infoscraper(url)
     weatherscraper(url)
     exit()
 
@@ -211,3 +235,4 @@ def main():
 # Call the main function
 if __name__ == "__main__":
     main()
+
